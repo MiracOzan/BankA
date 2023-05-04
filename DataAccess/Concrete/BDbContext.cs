859bd283055e5fbc07ab97e4ebc024d61b8669e6
Miraç Ozan T <m.ozan.tosun@gmail.com>
2023-05-04 18:54:36 +0300

    Add project files.

---
+++ DataAccess/Concrete/BDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class BDbContext : IdentityDbContext<AppUser, AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MIRAÇ;initial catalog=BankA;integrated Security = true;TrustServerCertificate=True");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+       
+    }
+}
---
+++ Entities/Concrete/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Entities.Concrete
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string District { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
