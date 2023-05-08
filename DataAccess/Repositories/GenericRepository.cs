ff9fee3eb93abca10ec3e1e554d72719877fc28d
Miraç Ozan T <m.ozan.tosun@gmail.com>
2023-05-08 14:19:31 +0300

    refactor(update Generic Repository)

---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var context = new BDbContext();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetById(int Id)
+        {
+            using var context = new BDbContext();
+            return context.Set<T>().Find(Id); 
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new BDbContext();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            using var context = new BDbContext();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new BDbContext();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+    }
+}
